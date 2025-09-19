bd0a5784ecf43f5822e4635fcdc6cf2887beb4bf
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2025-09-19 17:02:13 +0200

    fix: config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,20 +1,25 @@
 package config
 
 import (
+	"time"
+
 	"github.com/openkcm/common-sdk/pkg/commoncfg"
 )
 
 type Config struct {
 	commoncfg.BaseConfig `mapstructure:",squash"`
 
-	// gRPC server configuration
-	GRPCServer GRPCServer `yaml:"grpcServer"`
+	HTTP       HTTPServer `yaml:"http"`
+	GRPCServer GRPCServer `yaml:"grpc"`
 }
 
-// gRPC server configuration
+type HTTPServer struct {
+	Address         string        `yaml:"address" default:":8080"`
+	ShutdownTimeout time.Duration `yaml:"shutdownTimeout" default:"5s"`
+}
+
 type GRPCServer struct {
 	commoncfg.GRPCServer `mapstructure:",squash"`
-
 	// also embed client attributes for the gRPC health check client
 	Client commoncfg.GRPCClient
 }
