0de6f3a3f295255bd36c2d676b090250051b59c7
Chris Burkert <burkert.chris@gmail.com>
2025-10-01 09:56:40 +0200

    fix: Session Model (#20)

--- pkg/session/model.go
+++ pkg/session/model.go
@@ -2,19 +2,26 @@ package session
 
 import "time"
 
-type Session struct {
-	ID          string
-	TenantID    string
-	Fingerprint string
-	Token       string
-	Expiry      time.Time
+// State represents the state of an authentication process according to the OIDC spec.
+// It is used to align the auth request with the callback and to store necessary
+// information for completing the authentication process.
+type State struct {
+	ID           string    // State ID to align the auth request with the callback
+	TenantID     string    // Tenant ID for which the login is done
+	Fingerprint  string    // Fingerprint to bind the login to a specific client
+	PKCEVerifier string    // PKCE verifier to validate the PKCE challenge
+	RequestURI   string    // Request URI for the eventual redirect
+	Expiry       time.Time // Expiry time of the login process
 }
 
-type State struct {
-	ID           string
-	TenantID     string
-	Fingerprint  string
-	PKCEVerifier string
-	RequestURI   string
-	Expiry       time.Time
+// Session represents a user session in our system.
+type Session struct {
+	ID           string    // Session ID in our system
+	TenantID     string    // Tenant ID for which the session is created
+	Fingerprint  string    // Fingerprint to bind the session to a specific client
+	CSRFToken    string    // CSRF token to prevent CSRF attacks
+	Claims       string    // JSON string of claims from the ID token
+	AccessToken  string    // Access token from the identity provider
+	RefreshToken string    // Refresh token from the identity provider
+	Expiry       time.Time // Expiry time of the session
 }
