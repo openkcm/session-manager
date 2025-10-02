e061ae065b8c4c31a5e60b6a595652bd537c661c
Chris Burkert <burkert.chris@gmail.com>
2025-10-02 11:37:47 +0200

    feat: add stubs for gRPC services

--- internal/business/server/grpc_server.go
+++ internal/business/server/grpc_server.go
@@ -8,16 +8,27 @@ import (
 	"github.com/openkcm/common-sdk/pkg/health"
 	"github.com/samber/oops"
 
+	oidcmappingv1 "github.com/openkcm/api-sdk/proto/kms/api/cmk/sessionmanager/oidcmapping/v1"
+	oidcproviderv1 "github.com/openkcm/api-sdk/proto/kms/api/cmk/sessionmanager/oidcprovider/v1"
 	slogctx "github.com/veqryn/slog-context"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 
 	"github.com/openkcm/session-manager/internal/config"
+	"github.com/openkcm/session-manager/internal/grpc"
 )
 
-func StartGRPCServer(ctx context.Context, cfg *config.Config) error {
+func StartGRPCServer(ctx context.Context, cfg *config.Config,
+	oidcprovidersrv *grpc.OIDCProviderServer,
+	oidcmappingsrv *grpc.OIDCMappingServer,
+) error {
 	grpcServer := commongrpc.NewServer(ctx, &cfg.GRPC.GRPCServer)
 
+	// Register health server
 	healthpb.RegisterHealthServer(grpcServer, &health.GRPCServer{})
+	// Register OIDC provider server for ExtAuthZ
+	oidcproviderv1.RegisterOIDCProviderServer(grpcServer, oidcprovidersrv)
+	// Register OIDC mapping server for the regional tenant manager
+	oidcmappingv1.RegisterOIDCMappingServer(grpcServer, oidcmappingsrv)
 
 	listener, err := new(net.ListenConfig).Listen(ctx, "tcp", cfg.GRPC.Address)
 	if err != nil {
