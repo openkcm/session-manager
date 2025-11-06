cf0b00e751349a73a5921deb54f37a55ccc41c2e
Pawel Szelag <pablo.szel@gmail.com>
2025-11-06 14:47:14 +0100

    delete change on down migration

--- sql/00002_add_properties_to_providers.sql
+++ sql/00002_add_properties_to_providers.sql
@@ -1,7 +1,3 @@
 -- +goose Up
 ALTER TABLE oidc_providers
-    ADD COLUMN properties JSONB;
-
--- +goose Down
-ALTER TABLE oidc_providers
-DROP COLUMN IF EXISTS properties;
+    ADD COLUMN IF NOT EXISTS properties JSONB;
