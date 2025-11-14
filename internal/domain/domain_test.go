df8f89626860f10a47bd6741c47e4b4e3a276c1e
Chris Burkert <burkert.chris@gmail.com>
2025-11-14 09:44:24 +0100

    fix: set the cookies for the correct domain (#101)

---
+++ internal/domain/domain_test.go
@@ -0,0 +1,48 @@
+package domain
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestDomainFromRequest(t *testing.T) {
+	// create the test cases
+	tests := []struct {
+		name string
+		url  *url.URL
+		want string
+	}{
+		{
+			name: "normal case",
+			url: &url.URL{
+				Scheme: "https",
+				Host:   "example.com",
+			},
+			want: "https://example.com",
+		}, {
+			name: "more complex url",
+			url: &url.URL{
+				Scheme:   "https",
+				Host:     "example.com",
+				Opaque:   "/path?query=1",
+				Path:     "/foo",
+				Fragment: "section1",
+			},
+			want: "https://example.com",
+		},
+	}
+	// run the tests
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+
+			// Act
+			got := domainFromRequest(tc.url)
+
+			// Assert
+			if got != tc.want {
+				t.Errorf("domainFromRequest() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
