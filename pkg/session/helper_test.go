a0bdcdbdef8cb16ccdfe37da1d72bf69f1196c02
Danylo Shevchenko <39991717+alienvspredator@users.noreply.github.com>
2025-11-26 10:00:53 +0100

    feature/access token verification (#135)

--- pkg/session/helper_test.go
+++ pkg/session/helper_test.go
@@ -30,14 +30,14 @@ func StartOIDCServer(t *testing.T, fail bool) *httptest.Server {
 				JwksURI:               server.URL + "/.well-known/jwks.json",
 			})
 		case "/.well-known/jwks.json":
-			_, _ = w.Write([]byte(`{"keys":[{"kty": "RSA", "e": "AQAB", "use": "sig", "kid": "7cdrxOwDtBcW6ZmoW1CHjx2f74xqS6GAwJXOUd_oECw", "alg": "RS256", "n": "nMds_LftGh9YWfCuKfTU9rHezOPOUzooalZXIXMBnj4Xd7EQieVH4acwIlGQDsy9FasnSUzok4eeuJR1nmz7I5d0qIDjw_SItsFe83KetfFBLPsoCrR3kzcuof8KG3_N7pTGWMyl9cb8QTMzRYgzSrfgMJgi1TCHQq5uE-CWdjaCTklJgvnUb9QjYoyf3CkGz6hjlfu1TPw2CQfVXy0fW5jT9S6d10zYfYXnfeYxZFiKBgv2YNUPtwnejs0mZcE7lLyURf1tgkgZheHNde6Nz8UC0HEbGKBT6I-WXaFUJsmI5GDsQXTNfp6YmdYk_s-rM4bz-Hg51XI0JWk4J2bUyQ"}]}`))
+			_, _ = w.Write([]byte(`{"keys":[{"kty": "RSA", "e": "AQAB", "use": "sig", "kid": "MwK4iAYDIILiA_ymyjAwMGAaLlW84jOAqR0V-oojuIk", "alg": "RS256", "n": "nD89GVZMXuv_MSbH_SqDnU5oQgLlcH6yGe5LkXSdP_UzBXt49wPRoVHE-W981oylw9vhzfNBE8JY0PSkxVvYCWwYP86YWVtJix23iONYpXeAH9M1ep4Gzo1y0XnjAKURi-sN5T5nUBZ-fkODvyr6ALIUG3AXzaRow1RMmhUOx1spKGS34DJPv0D3E6aVcGkwgUwZcBhObYxGQdMAYi-OYDDS3uAkFciO3G1Bpz4nyW_JaV7i4zkMOH6-2wYFt1fjMsyc0lt1eRqdUVdANy0kDtmIXnjgjKN0Isr16flzfRDXfOQmaBPp14hQPiAgVFaqvTIvXucXOkiWcAQWhas2Aw"}]}`))
 		case "/oauth2/token":
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			tokenResponse := session.TokenResponse{
-				AccessToken:  "access-token",
+				AccessToken:  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.m3RtvePjRxhO0-O2FOzJvYWUNZqxdQ5p4wI5YTjC8BmFzCH-2gMjk1RSG00-_q0PkcKqoD_hZFVub88298nhF7WpEj2pEWvDhWeG4g3H4JxcFw-a2Pwam80qdrOOA8NkmDtTewC90yshYcGktGMHk5jjfh4sKRaZz9FmkBpc2G9I1NyxcCyj9yatMu64yFDNa0-CbaSsWyCFgsKvNxM944nJkT7q3OLFz_Tgn4HSXExEDE_Xkwhz6zykg12tcU9-5Fk40yEfOEaBCTmuv3AMguBOlEBD1X2IsPcz03My5bpECEFIuRbqu-xvny1vEhzjNB565uk4Es9PdLwi_6frWA",
 				RefreshToken: "refresh-token",
-				IDToken:      "eyJhbGciOiJSUzI1NiIsImtpZCI6IjdjZHJ4T3dEdEJjVzZabW9XMUNIangyZjc0eHFTNkdBd0pYT1VkX29FQ3ciLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiJqd3QtdGVzdCIsImp0aSI6IjIzNDE0MzUiLCJuYmYiOjE3NjA1NzcwMjgsImV4cCI6MTc2MDU4NzgyOCwiaWF0IjoxNzYwNTc3MDI4LCJpc3MiOiJkYXJ3aW5MYWJzIiwiYXVkIjoiaHR0cDovL3d3dy5kYXJ3aW4tbGFicy5jb20ifQ.TbUiSRxNE-x2NYc_9CkLt59caV_CeOxaaHjbtBekWeKSnYXlZIOqf6qikdVhKwN3IdssUi5af6E2tVEvM4fAZuCGKy7qkHXqvitxm2XLfZPvQzscrN7L476rjUaEr2HcjqoOmhPwcgTfeJJRp9o_JIqvtb-NXhIZPbPBkinTWFIArLfcJ1WZx4fYbXY7nixunJfQqYYtZSP_OukzRbAK5qwPj55USPFhh3IBWrUsS4x_YOiF8PITldLhCYIFNmhI5vkT6KwaWVYAVZPnwLARSW0nZAKnv_qAuhwHbhP8Et746Qw-WF-5K2Ij3YlgsNG-6_c0ID2MwBhoqpg-1sFcug",
+				IDToken:      "eyJhbGciOiJSUzI1NiIsImtpZCI6Ik13SzRpQVlESUlMaUFfeW15akF3TUdBYUxsVzg0ak9BcVIwVi1vb2p1SWsiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiJqd3QtdGVzdCIsImp0aSI6IjIzNDE0MzUiLCJhdF9oYXNoIjoicVJiNThaanpTZFByYnpGQ3hielJFZyIsIm5iZiI6MTc2NDExNDM3MSwiZXhwIjoxNzY0MTI1MTcxLCJpYXQiOjE3NjQxMTQzNzEsImlzcyI6InNhcCIsImF1ZCI6Imh0dHBzOi8vZXhhbXBsZS5jb20ifQ.JhC2oGYRHTL4NVaz1CZKWop_Iq54fxQOQL2pJap1LIMFKRz9RqgZr_WMulBLjNxppS3v5KFaMMp28YirzhzJQVbIlrEuUQZQCeODmLYSVkyeQKGb9WTSirzZInZbICjfocgppSzZ_Z8_P0GSS_h4IEFgcK0jnfb-2O_Xef1dYSoxA-sOFCxvn48jnjBLNjRQh2uYY61unJRzAbchXTBCtTSKNL1SEM4rCvV9b9dfYKBSlaQ11DKzzC1Zd5xG4JNkrbDXYu6MAxYLz_getXsQh6rVqOnMjUOMQLjUcuMuSva1Fh9gCeJNWsy34bh6lfScBb67L3i5D1s8pciLYTNMDQ",
 				TokenType:    "Bearer",
 				ExpiresIn:    3600,
 			}
