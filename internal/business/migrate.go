b5f9f76a04d81f93370cf77258c6f6570d4524fd
Chris Burkert <burkert.chris@gmail.com>
2025-12-03 16:37:27 +0100

    chore: refact cmd structure to reuse code

---
+++ internal/business/migrate.go
@@ -0,0 +1,41 @@
+package business
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/pressly/goose/v3"
+	"github.com/samber/oops"
+
+	// Register pgx driver
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"github.com/openkcm/session-manager/internal/config"
+	migrations "github.com/openkcm/session-manager/sql"
+)
+
+// MigrateMain starts the database migration
+func MigrateMain(ctx context.Context, cfg *config.Config) error {
+	connStr, err := config.MakeConnStr(cfg.Database)
+	if err != nil {
+		return fmt.Errorf("making connection string from config: %w", err)
+	}
+
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		return oops.In("main").Wrapf(err, "opening DB connection")
+	}
+
+	goose.SetBaseFS(migrations.FS)
+
+	if err := goose.SetDialect("pgx"); err != nil {
+		return fmt.Errorf("setting goose dialect: %w", err)
+	}
+
+	if err := goose.UpContext(ctx, db, "."); err != nil {
+		return fmt.Errorf("applying migrations: %w", err)
+	}
+
+	return nil
+}
